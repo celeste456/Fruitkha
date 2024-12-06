2256d997b32ad95a8d8856cd68647534d10b4769
celeste456 <85599011+celeste456@users.noreply.github.com>
2024-12-06 00:14:00 -0600

    Entity Auth

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -4,6 +4,8 @@ using BackEnd.Services.Interfaces;
 using DAL.Implementations;
 using DAL.Interfaces;
 using Entities.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +17,38 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 #region DI
-builder.Services.AddDbContext<FruitkhaContext>();
+builder.Services.AddDbContext<FruitkhaContext>(options =>
+                    options.UseSqlServer(
+                        builder
+                        .Configuration
+                        .GetConnectionString("DefaulConnection")
+                        ));
+
+builder.Services.AddDbContext<AuthDBContext>(options =>
+                    options.UseSqlServer(
+                        builder
+                        .Configuration
+                        .GetConnectionString("DefaulConnection")
+                        ));
+
+builder.Services.AddIdentityCore<IdentityUser>()
+             .AddRoles<IdentityRole>()
+            .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("fide")
+            .AddEntityFrameworkStores<AuthDBContext>()
+            .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+
+});
+
+builder.Services.AddAuthentication();
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IImageHandler, ImageHandler>();
 
@@ -36,6 +69,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
--- Entities/Entities/FruitkhaContext.cs
+++ Entities/Entities/FruitkhaContext.cs
@@ -30,9 +30,9 @@ public partial class FruitkhaContext : DbContext
     public virtual DbSet<ShoppingCart> ShoppingCarts { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=Fruitkha;Integrated Security=True;Trusted_Connection=True; TrustServerCertificate=True;");
-
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Category>(entity =>
